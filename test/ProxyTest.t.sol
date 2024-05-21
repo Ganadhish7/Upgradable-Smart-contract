842042bae524ebd7c74287cf26733122044c4bdd
Ganadhish7 <ganadhishmardikar7160@gmail.com>
2024-05-21 21:31:21 +0530

    update proxyTest

--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import "../src/ProxyContract.sol";
 import "../src/ImplementationContract.sol";
+import "../src/ImplementationContractDeposit.sol";
 
 // Define a contract named ProxyTest for testing the functionality of a proxy contract.
 contract ProxyTest  {
@@ -33,6 +34,24 @@ contract ProxyTest  {
         require(implementationAddress == newImplementation, "Implementation address should be Added");
     }
 
+    // Test function to verify adding a new implementation address to the proxy.
+    function testAddImplementationDeposit() public {
+        // Calculate the function selector for the "transfer" function using keccak256.
+        bytes4 functionSelector = bytes4(keccak256("deposit(uint256)"));
+
+        // Create a new instance of the ImplementationContract with a specific address.
+        address newImplementation = address(new DepositContract(0x123));
+
+        // Add the new implementation to the proxy for the specified function.
+        proxy.addImplementation(functionSelector, newImplementation);
+
+        // Retrieve the implementation address for the specified function from the proxy.
+        address implementationAddress = proxy.implementationAddresses(functionSelector);
+
+        // Assert that the implementation address added is the expected one.
+        require(implementationAddress == newImplementation, "Implementation address should be Added");
+    }
+
      // Test function to verify updating an existing implementation address in the proxy.
     function testUpdateImplementation() public {
         // Similar to testAddImplementation, but this time updating an existing implementation.
@@ -51,8 +70,47 @@ contract ProxyTest  {
         require(implementationAddress == newImplementation, "Implementation address should be updated");
     }
 
+    // Test function to verify adding a new implementation address to the proxy.
+    function testUpdateImplementationDeposit() public {
+        // Calculate the function selector for the "transfer" function using keccak256.
+        bytes4 functionSelector = bytes4(keccak256("deposit(uint256)"));
+
+        // Create a new instance of the ImplementationContract with a specific address.
+        address newImplementation = address(new DepositContract(0x123));
+
+        // Add the new implementation to the proxy for the specified function.
+        proxy.updateImplementation(functionSelector, newImplementation);
+
+        // Retrieve the implementation address for the specified function from the proxy.
+        address implementationAddress = proxy.implementationAddresses(functionSelector);
+
+        // Assert that the implementation address added is the expected one.
+        require(implementationAddress == newImplementation, "Implementation address should be Added");
+    }
+
      // Test function to verify removing an implementation address from the proxy.
     function testRemoveImplementation() public {
+        // Similar to testAddImplementation, but this time removing the implementation.
+        bytes4 functionSelector = bytes4(keccak256("transfer(address,uint256)"));
+
+        // Create a new instance of the ImplementationContract with a specific address.
+        address newImplementation = address(new ImplementationContract(0x123));
+
+        // Add the new implementation to the proxy for the specified function.
+        proxy.addImplementation(functionSelector, newImplementation);
+
+        // Remove the implementation for the specified function from the proxy.
+        proxy.removeImplementation(functionSelector);
+
+        // Retrieve the implementation address for the specified function from the proxy.
+        address implementationAddress = proxy.implementationAddresses(functionSelector);
+
+        // Assert that the implementation address has been removed.
+        require(implementationAddress == address(0), "Implementation address should be removed");
+    }
+
+    // Test function to verify removing an implementation address from the proxy.
+    function testRemoveImplementationDeposit() public {
         // Similar to testAddImplementation, but this time removing the implementation.
         bytes4 functionSelector = bytes4(keccak256("transfer(address,uint256)"));
 
@@ -81,9 +139,18 @@ contract ProxyTest  {
         // proxy.addImplementation(functionSelector, newImplementation); 
 
         // Simulate a call to the proxy contract's fallback function with the transfer function selector
-        (bool success, ) = address(proxy).call(abi.encodeWithSelector(functionSelector, address(proxy), 0x123));
+        (bool success, ) = address(proxy).delegatecall(abi.encodeWithSelector(functionSelector, address(proxy), 0x123));
 
         // Use require for assertion
+        require(success, "Fallback function should successfully delegate the call");
+    }
+
+    function testFallbackDelegationImplementationDeposit() public {
+
+        bytes4 functionSelector = bytes4(keccak256("deposit(uint256)"));
+
+        (bool success, ) = address(proxy).delegatecall(abi.encodeWithSelector(functionSelector, address(proxy), 0x123));
+
         require(success, "Fallback function should successfully delegate the call");
     }
 }
